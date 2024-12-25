59763387d18731c505ce851079195618cd72cf6a
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2024-12-25 22:17:27 +0530

    Sample project to simulate the issue https://github.com/dotnet/maui/issues/26804

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# maui-issue-26804
+## Horizontal misalignment of CollectionView items - Content is missing when the value is set to End
+
 Sample project to simulate the issue https://github.com/dotnet/maui/issues/26804
---
+++ src/MauiApp1/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            UserAppTheme = PlatformAppTheme;
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new MainPage());
+            window.Title = "MauiApp1";
+            return window;
+        }
+    }
+}
